03720a12f83ecb7201f1e95e24447aa02f6b7ba3
Jayfunf <simh3077@gmail.com>
2021-07-27 16:58:36 +0900

    first commit

---
+++ To_Do_Quest/To_Do_Quest/To_Do_QuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_QuestApp.swift
+//  To_Do_Quest
+//
+//  Created by Minhyun Cho on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_QuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
